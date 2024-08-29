6a9fe655998ba7558a986915e4c8b83662751497
kumjh1122 <kumjh1122@gmail.com>
2024-08-29 16:10:49 +0900

    test 커밋 : 정책 시행에 의한 test 커밋

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const placeRoutes = require("./place");
-const workRoutes = require("./work");
-const imageRoutes = require("./image");
+const express = require('express');
+const placeRoutes = require('./place');
+const workRoutes = require('./work');
+const imageRoutes = require('./image');
 const {
   getEpisodesBySeason,
   getLocationByEpisode,
-} = require("../controllers/work");
+} = require('../controllers/work');
 
 const router = express.Router();
-
-router.use("/place", placeRoutes);
-router.use("/work", workRoutes);
-router.use("/image", imageRoutes);
+// 20240829 test commit
+router.use('/place', placeRoutes);
+router.use('/work', workRoutes);
+router.use('/image', imageRoutes);
 
 module.exports = router;
